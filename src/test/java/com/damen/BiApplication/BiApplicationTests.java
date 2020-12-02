b122982abd57c0229a895407516d2c85c48fd243
Magued Mamdouh <maguedmamdouh@outlook.com>
2020-12-02 09:46:25 +0200

    Fixing Imports

---
+++ src/test/java/com/damen/BiApplication/BiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damen.BiApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
